ffbd94a5ef00ae1375960aea08b46ca5e279900f
GyeongHoKim <rlarudgh2017@gmail.com>
2020-11-22 06:15:24 +0000

    practice 11

--- AddressBook/ImplementByClassMethod/MyList.cpp
+++ AddressBook/ImplementByClassMethod/MyList.cpp
@@ -1,7 +1,22 @@
 #include "MyList.h"
+#include "UserData.h"
+#include <stdio.h>
+#include <cstring>
 
-CMyList::CMyList() {}
+CMyList::CMyList(void) {}
 
-CMyList::~CMyList() {}
+CMyList::~CMyList(void) {}
+
+int CMyList::AddNewNode(const char *pszName, const char *pszPhone)
+{
+    CUserData *pNewUser = NULL;
+
+    if(FindNode(pszName) != NULL)
+        return 0;
+
+    pNewUser = new CUserData;
+    strcpy(pNewUser->szName, sizeof(pNewUser->szName), pszName);
+    strcpy(pNewUser->szPhone, sizeof(pNewUser->szPhone), pszPhone);
+}
 
 void CMyList::PrintAll(void) {}
