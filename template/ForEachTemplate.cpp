0a57272bad31ca4504a34430b1ec3eee09b70175
GyeongHo Kim <GyeongHoKim@users.noreply.github.com>
2020-11-28 12:09:35 +0900

    for_each() template version

---
+++ template/ForEachTemplate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+include <string>
+using namespace std;
+
+template<typename IterT, typename Func>
+void For_each(IterT bein, IterT end, Func pf)
+{
+    while(begin != end)
+        pf(*begin++);
+}
+
+void PrintInt(int data)
+{
+    cout << data << " ";
+}
+
+void PrintString(string data)
+{
+    cout << data << " ";
+}
+
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    For_each(arr, arr+5, PrintInt);
+    cout << endl;
+    
+    string sarr[3] = {"abc", "ABCDE", "Hello!"};
+    For_each(arr, arr+3, PrintString);
+    cout << endl;
+    
+    return 0;
+}
