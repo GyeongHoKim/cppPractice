6613729c3b7b5b7c178ceeb1c44d12366cca4786
GyeongHoKim <rlarudgh2017@gmail.com>
2020-11-28 05:14:15 +0000

    answer of 4-1

--- answerPractice/4-1.cpp
+++ answerPractice/4-1.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
+#include <algorithm>
 
 template<typename T>
 class Copy
 {
 public:
-    void operator() (T* t, T* s, int n)
+    void operator() (T t, T s, int n)
     {
-        for_each(t, t+n, arrcpy(t, s));
-    }
-    
-    void arrcpy(T* t, T* s)
-    {
-        *t = *s;
+        for(int i = 0; i < 5; i++)
+            *(t + i) = *(s + i);
     }
 };
 
@@ -19,7 +16,10 @@ int main()
 {
     int arr1[5] = {10, 20, 30, 40, 50};
     int arr2[5];
-    Copy(arr2, arr1, 5);
+    Copy<int*> copy;
+    copy(arr2, arr1, 5);
     
+    for(int i = 0; i < 5; ++i) std::cout << arr2[i] << " ";
+
     return 0;
 }
